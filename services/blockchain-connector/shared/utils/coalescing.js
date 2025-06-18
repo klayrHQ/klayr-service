d836840ed15f71359c4f325d7374c28c6327bb96
Aldo Suhartono Putra <aldhosutra@gmail.com>
2025-06-18 14:36:14 +0700

    fix coalescer logger

--- services/blockchain-connector/shared/utils/coalescing.js
+++ services/blockchain-connector/shared/utils/coalescing.js
@@ -3,6 +3,8 @@ const { Logger } = require('klayr-service-framework');
 const config = require('../../config');
 const { ReqoalInstance } = require('reqoal');
 
+const logger = Logger();
+
 // constants
 const DEFAULT_TTL_MS = config.coalescingTTL;
 const PRUNE_INTERVAL_MS = 60000;
@@ -12,7 +14,7 @@ let coalescer;
 
 const getCoalescerInstance = () => {
 	if (!coalescer) {
-		coalescer = new ReqoalInstance(PRUNE_INTERVAL_MS, DEFAULT_TTL_MS, Logger);
+		coalescer = new ReqoalInstance(PRUNE_INTERVAL_MS, DEFAULT_TTL_MS, logger);
 	}
 	return coalescer;
 };
