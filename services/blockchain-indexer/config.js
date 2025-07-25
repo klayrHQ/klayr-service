caaf2085434fe3f831dbe59d72c3cbd3be5dc526
Aldo Suhartono Putra <aldhosutra@gmail.com>
2025-07-25 16:10:51 +0700

    add config.supplyIndexing.blockFrequency for supply indexing

--- services/blockchain-indexer/config.js
+++ services/blockchain-indexer/config.js
@@ -231,4 +231,16 @@ config.invokeAllowedMethods = process.env.INVOKE_ALLOWED_METHODS
 
 config.indexBlocksRetryDelay = Number(process.env.INDEX_BLOCKS_RETRY_DELAY) || 1000;
 
+/**
+ * Supply indexing config
+ */
+
+/**
+ * NOTE:
+ * config.supplyIndexing.blockFrequency of -1 means that supply indexing will only occur after initial indexing finished 100%
+ * this could speed up initial block indexing, but may break supply indexing if indexing unexpectedly terminated halfway
+ */
+
+config.supplyIndexing.blockFrequency = Number(process.end.INDEX_SUPPLY_BLOCK_FREQUENCY) || 1; // Default to every block
+
 module.exports = config;
