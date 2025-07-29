82653287bce6df69cd2775f2d5ea553bce18fbfc
Aldo Suhartono Putra <aldhosutra@gmail.com>
2025-07-29 10:12:37 +0700

    refactor getLastIndexedBlock & setLastIndexedBlock to separate file

--- services/blockchain-indexer/shared/indexer/blockchainIndex.js
+++ services/blockchain-indexer/shared/indexer/blockchainIndex.js
@@ -79,6 +79,7 @@ const transactionsTableSchema = require('../database/schema/transactions');
 const validatorsTableSchema = require('../database/schema/validators');
 const { normalizeBlock } = require('../dataService/business/blocks');
 const { indexTokenSupply } = require('./supplyIndexer');
+const { getLastIndexedBlock, setLastIndexedBlock } = require('./lastIndexedBlock');
 
 const MYSQL_ENDPOINT = config.endpoints.mysql;
 
@@ -92,44 +93,9 @@ const getValidatorsTable = () => getTableInstance(validatorsTableSchema, MYSQL_E
 
 const validateBlock = block => !!block && block.height >= 0;
 
-const LAST_INDEXED_BLOCK_CACHE_KEY = 'lastIndexedBlock';
 const LARGEST_MISSING_BLOCK_HEIGHT_CACHE_KEY = 'largestMissingBlockHeight';
 
-const lastIndexedBlockCache = CacheLRU('lastIndexedBlock', { max: 1 });
 const largestMissingBlockHeightCache = CacheLRU('largestMissingBlockHeight', { max: 1 });
-
-const getLastIndexedBlockFromDB = async () => {
-	const blocksTable = await getBlocksTable();
-	const [lastIndexedBlockFromDB] = await blocksTable.find(
-		{
-			sort: 'height:desc',
-			limit: 1,
-		},
-		Object.getOwnPropertyNames(blocksTableSchema.schema),
-	);
-	if (lastIndexedBlockFromDB) await setLastIndexedBlock(lastIndexedBlockFromDB);
-	return lastIndexedBlockFromDB;
-};
-
-const getLastIndexedBlock = async () => {
-	const lastIndexedBlock = await lastIndexedBlockCache.get(LAST_INDEXED_BLOCK_CACHE_KEY);
-	if (lastIndexedBlock) {
-		return JSON.parse(lastIndexedBlock);
-	} else {
-		return await getLastIndexedBlockFromDB();
-	}
-};
-
-const setLastIndexedBlock = async block => {
-	if (block) {
-		await lastIndexedBlockCache.set(
-			LAST_INDEXED_BLOCK_CACHE_KEY,
-			JSON.stringify({ id: block.id, height: block.height }),
-		);
-	} else {
-		await getLastIndexedBlockFromDB();
-	}
-};
 
 const setLargestMissingBlockHeight = async missingBlockHeight => {
 	await largestMissingBlockHeightCache.set(
---
+++ services/blockchain-indexer/shared/indexer/lastIndexedBlock.js
@@ -0,0 +1,51 @@
+const {
+	DB: {
+		MySQL: { getTableInstance },
+	},
+	CacheLRU,
+} = require('klayr-service-framework');
+
+const config = require('../../config');
+const MYSQL_ENDPOINT = config.endpoints.mysql;
+
+const blocksTableSchema = require('../database/schema/blocks');
+
+const getBlocksTable = () => getTableInstance(blocksTableSchema, MYSQL_ENDPOINT);
+
+const LAST_INDEXED_BLOCK_CACHE_KEY = 'lastIndexedBlock';
+const lastIndexedBlockCache = CacheLRU('lastIndexedBlock', { max: 1 });
+
+const getLastIndexedBlockFromDB = async () => {
+	const blocksTable = await getBlocksTable();
+	const [lastIndexedBlockFromDB] = await blocksTable.find(
+		{
+			sort: 'height:desc',
+			limit: 1,
+		},
+		Object.getOwnPropertyNames(blocksTableSchema.schema),
+	);
+	if (lastIndexedBlockFromDB) await setLastIndexedBlock(lastIndexedBlockFromDB);
+	return lastIndexedBlockFromDB;
+};
+
+const getLastIndexedBlock = async () => {
+	const lastIndexedBlock = await lastIndexedBlockCache.get(LAST_INDEXED_BLOCK_CACHE_KEY);
+	if (lastIndexedBlock) {
+		return JSON.parse(lastIndexedBlock);
+	} else {
+		return await getLastIndexedBlockFromDB();
+	}
+};
+
+const setLastIndexedBlock = async block => {
+	if (block) {
+		await lastIndexedBlockCache.set(
+			LAST_INDEXED_BLOCK_CACHE_KEY,
+			JSON.stringify({ id: block.id, height: block.height }),
+		);
+	} else {
+		await getLastIndexedBlockFromDB();
+	}
+};
+
+module.exports = { getLastIndexedBlock, setLastIndexedBlock };
