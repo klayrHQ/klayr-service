3bc03790af4d6e77357ab29f4b687b594a1c330f
Aldo Suhartono Putra <aldhosutra@gmail.com>
2025-08-06 11:30:49 +0700

    add isEnded check to indexSpeed benchmark

--- services/blockchain-indexer/shared/utils/indexSpeed.js
+++ services/blockchain-indexer/shared/utils/indexSpeed.js
@@ -3,11 +3,12 @@ const { Logger } = require('klayr-service-framework');
 let blocksIndexedThisSecond = 0;
 let maxThroughput = 0;
 let indexingSpeedInterval = null;
+let isEnded = false;
 
 const logger = Logger();
 
 const startIndexSpeedRecord = () => {
-	if (!indexingSpeedInterval) {
+	if (!indexingSpeedInterval && !isEnded) {
 		indexingSpeedInterval = setInterval(() => {
 			logger.info(
 				`Block Indexing Speed - Current throughput: ${blocksIndexedThisSecond} blocks/sec, Maximum achieved throughput: ${maxThroughput} blocks/sec`,
@@ -27,6 +28,7 @@ const increaseBlockIndexedForSpeedRecord = () => {
 const stopIndexSpeedRecord = () => {
 	if (indexingSpeedInterval) {
 		clearInterval(indexingSpeedInterval);
+		isEnded = true;
 		indexingSpeedInterval = null;
 		logger.info(
 			`Block Indexing Speed - Ended: Maximum achieved throughput: ${maxThroughput} blocks/sec`,
