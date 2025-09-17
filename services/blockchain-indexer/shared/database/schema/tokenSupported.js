97aedf982f6218c175d806ca76dbf656c874d798
Aldo Suhartono Putra <aldhosutra@gmail.com>
2025-09-17 12:11:48 +0700

    fix null primary key on token_supported table

--- services/blockchain-indexer/shared/database/schema/tokenSupported.js
+++ services/blockchain-indexer/shared/database/schema/tokenSupported.js
@@ -3,7 +3,7 @@ module.exports = {
 	primaryKey: ['tokenID', 'chainID'],
 	schema: {
 		tokenID: { type: 'string', length: 16, null: false, defaultValue: '*' },
-		chainID: { type: 'string', length: 8, null: true },
+		chainID: { type: 'string', length: 8, null: false, defaultValue: '*' },
 	},
 	indexes: {},
 	purge: {},
