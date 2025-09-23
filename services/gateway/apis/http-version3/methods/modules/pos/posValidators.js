5d0b4edb9eca042d31d8bd877f38bdfc703ce48c
Aldo Suhartono Putra <aldhosutra@gmail.com>
2025-09-23 17:56:34 +0700

    change /pos/validators max limit to 1000

--- services/gateway/apis/http-version3/methods/modules/pos/posValidators.js
+++ services/gateway/apis/http-version3/methods/modules/pos/posValidators.js
@@ -52,7 +52,7 @@ module.exports = {
 			pattern: regex.PARTIAL_SEARCH,
 			altSwaggerKey: 'searchByNameAddressPubKey',
 		},
-		limit: { optional: true, type: 'number', min: 1, max: 103, default: 10 },
+		limit: { optional: true, type: 'number', min: 1, max: 1000, default: 10 },
 		offset: { optional: true, type: 'number', min: 0, default: 0 },
 		sort: {
 			optional: true,
