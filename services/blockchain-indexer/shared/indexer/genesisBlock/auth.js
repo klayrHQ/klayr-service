414fe7f66aae5694c187d702d2d4c01599f96749
Aldo Suhartono Putra <aldhosutra@gmail.com>
2025-10-13 19:22:11 +0700

    improve memory efficiency by adding genesis job directly in batch fragment

--- services/blockchain-indexer/shared/indexer/genesisBlock/auth.js
+++ services/blockchain-indexer/shared/indexer/genesisBlock/auth.js
@@ -1,12 +1,13 @@
 const { Logger } = require('klayr-service-framework');
 const { MODULE, MODULE_SUB_STORE } = require('../../constants');
 
-const requestAll = require('../../utils/requestAll');
 const { requestConnector } = require('../../utils/request');
 const { getKlayr32AddressFromHexAddress } = require('../../utils/account');
 const { addGenesisBlockJob } = require('./queue');
 
 const logger = Logger();
+const BATCH_SIZE = 10000;
+const BATCH_RETRY_DELAY = 1000;
 
 const indexAuthModuleAssets = async dbTrx => {
 	logger.info('Starting to index the genesis assets from the auth module.');
@@ -18,28 +19,46 @@ const indexAuthModuleAssets = async dbTrx => {
 	if (Object.keys(genesisBlockAssetsLength).includes(MODULE.AUTH)) {
 		const totalAuthData = genesisBlockAssetsLength[MODULE.AUTH][MODULE_SUB_STORE.AUTH.DATA];
 
-		const authModuleData = await requestAll(
-			requestConnector,
-			'getGenesisAssetByModule',
-			{ module: MODULE.AUTH, subStore: MODULE_SUB_STORE.AUTH.DATA, limit: 10000 },
-			totalAuthData,
-		);
+		for (let offset = 0; offset < totalAuthData; ) {
+			try {
+				const authModuleData = await requestConnector('getGenesisAssetByModule', {
+					module: MODULE.AUTH,
+					subStore: MODULE_SUB_STORE.AUTH.DATA,
+					limit: 10000,
+					offset,
+				});
 
-		const authDataSubstoreInfos = authModuleData[MODULE_SUB_STORE.AUTH.DATA];
+				const authDataSubstoreInfos = authModuleData[MODULE_SUB_STORE.AUTH.DATA];
 
-		for (let i = 0; i < authDataSubstoreInfos.length; i++) {
-			const { address, authAccount } = authDataSubstoreInfos[i];
+				for (let i = 0; i < authDataSubstoreInfos.length; i++) {
+					const { address, authAccount } = authDataSubstoreInfos[i];
 
-			const addressFormatted =
-				address.length !== 20 * 2 ? getKlayr32AddressFromHexAddress(address) : address;
+					const addressFormatted =
+						address.length !== 20 * 2 ? getKlayr32AddressFromHexAddress(address) : address;
 
-			await addGenesisBlockJob('indexGenesisAuthAccount', {
-				address: addressFormatted,
-				nonce: authAccount.nonce,
-				numberOfSignatures: authAccount.numberOfSignatures,
-				mandatoryKeys: authAccount.mandatoryKeys,
-				optionalKeys: authAccount.optionalKeys,
-			});
+					await addGenesisBlockJob('indexGenesisAuthAccount', {
+						address: addressFormatted,
+						nonce: authAccount.nonce,
+						numberOfSignatures: authAccount.numberOfSignatures,
+						mandatoryKeys: authAccount.mandatoryKeys,
+						optionalKeys: authAccount.optionalKeys,
+					});
+				}
+
+				const percent =
+					totalAuthData > 0 ? (((offset + BATCH_SIZE) / totalAuthData) * 100).toFixed(1) : 0;
+				logger.info(
+					`Scheduled ${Math.min(
+						offset + BATCH_SIZE,
+						totalAuthData,
+					)} of ${totalAuthData} auth item (${percent}%)`,
+				);
+
+				offset += BATCH_SIZE;
+			} catch (err) {
+				await new Promise(resolve => setTimeout(resolve, BATCH_RETRY_DELAY));
+				logger.warn(`Retrying indexGenesisAuthAccount batch starting at offset ${offset}...`);
+			}
 		}
 	}
 
--- services/blockchain-indexer/shared/indexer/genesisBlock/token.js
+++ services/blockchain-indexer/shared/indexer/genesisBlock/token.js
@@ -7,7 +7,6 @@ const {
 
 const { requestConnector } = require('../../utils/request');
 const { MODULE, MODULE_SUB_STORE } = require('../../constants');
-const requestAll = require('../../utils/requestAll');
 const { updateTotalLockedAmounts } = require('../utils/blockchainIndex');
 const { initSupportedTokens } = require('../../dataService/recorder/token/supported');
 
@@ -15,6 +14,8 @@ const config = require('../../../config');
 const { addGenesisBlockJob } = require('./queue');
 
 const MYSQL_ENDPOINT = config.endpoints.mysql;
+const BATCH_SIZE = 10000;
+const BATCH_RETRY_DELAY = 1000;
 
 const logger = Logger();
 
@@ -25,114 +26,207 @@ const indexTokenModuleAssets = async dbTrx => {
 	});
 
 	if (Object.keys(genesisBlockAssetsLength).includes(MODULE.TOKEN)) {
+		let numBalanceEntries = 0;
+		let numLockedEntries = 0;
+		let numSupplyEntries = 0;
+		let numEscrowedEntries = 0;
+		let numSupportedEntries = 0;
+
+		const lockedChangeMap = {};
+
 		const totalUsers = genesisBlockAssetsLength[MODULE.TOKEN][MODULE_SUB_STORE.TOKEN.USER];
 		const totalSupplyItem = genesisBlockAssetsLength[MODULE.TOKEN][MODULE_SUB_STORE.TOKEN.SUPPLY];
 		const totalEscrowItem = genesisBlockAssetsLength[MODULE.TOKEN][MODULE_SUB_STORE.TOKEN.ESCROW];
 		const totalSupportedItem =
 			genesisBlockAssetsLength[MODULE.TOKEN][MODULE_SUB_STORE.TOKEN.SUPPORTED];
 
-		const tokenUserModuleData = await requestAll(
-			requestConnector,
-			'getGenesisAssetByModule',
-			{ module: MODULE.TOKEN, subStore: MODULE_SUB_STORE.TOKEN.USER, limit: 10000 },
-			totalUsers,
-		);
-
-		const tokenSupplyModuleData = await requestAll(
-			requestConnector,
-			'getGenesisAssetByModule',
-			{ module: MODULE.TOKEN, subStore: MODULE_SUB_STORE.TOKEN.SUPPLY, limit: 10000 },
-			totalSupplyItem,
-		);
-
-		const tokenEscrowedModuleData = await requestAll(
-			requestConnector,
-			'getGenesisAssetByModule',
-			{ module: MODULE.TOKEN, subStore: MODULE_SUB_STORE.TOKEN.ESCROW, limit: 10000 },
-			totalEscrowItem,
-		);
-
-		const tokenSupportedModuleData = await requestAll(
-			requestConnector,
-			'getGenesisAssetByModule',
-			{ module: MODULE.TOKEN, subStore: MODULE_SUB_STORE.TOKEN.SUPPORTED, limit: 10000 },
-			totalSupportedItem,
-		);
-
-		const userSubStoreInfos = tokenUserModuleData[MODULE_SUB_STORE.TOKEN.USER];
-		const supplySubstoreInfos = tokenSupplyModuleData[MODULE_SUB_STORE.TOKEN.SUPPLY];
-		const escrowSubstoreInfos = tokenEscrowedModuleData[MODULE_SUB_STORE.TOKEN.ESCROW];
-		const supportedSubstoreInfos = tokenSupportedModuleData[MODULE_SUB_STORE.TOKEN.SUPPORTED];
-
-		let numLockedEntries = 0;
-		const lockedChangeMap = {};
-
-		// eslint-disable-next-line no-restricted-syntax
-		for (let i = 0; i < userSubStoreInfos.length; i++) {
-			const { address, tokenID, availableBalance, lockedBalances } = userSubStoreInfos[i];
-
-			// Add entry to index the genesis token balances
-			await addGenesisBlockJob('indexGenesisTokenBalances', {
-				address,
-				tokenID,
-				availableBalance,
-			});
-
-			// eslint-disable-next-line no-restricted-syntax
-			for (let k = 0; k < lockedBalances.length; k++) {
-				const lockedBalance = lockedBalances[k];
-				if (!lockedChangeMap[tokenID]) lockedChangeMap[tokenID] = BigInt(0);
-				lockedChangeMap[tokenID] += BigInt(lockedBalance.amount);
-
-				// Add entry to index the genesis token locked
-				await addGenesisBlockJob('indexGenesisTokenLocked', {
-					address,
-					tokenID,
-					module: lockedBalance.module,
-					amount: lockedBalance.amount,
+		for (let offset = 0; offset < totalUsers; ) {
+			try {
+				const tokenUserModuleData = await requestConnector('getGenesisAssetByModule', {
+					module: MODULE.TOKEN,
+					subStore: MODULE_SUB_STORE.TOKEN.USER,
+					limit: BATCH_SIZE,
+					offset,
 				});
+
+				const userSubStoreInfos = tokenUserModuleData[MODULE_SUB_STORE.TOKEN.USER];
+
+				// eslint-disable-next-line no-restricted-syntax
+				for (let i = 0; i < userSubStoreInfos.length; i++) {
+					const { address, tokenID, availableBalance, lockedBalances } = userSubStoreInfos[i];
+
+					// Add entry to index the genesis token balances
+					await addGenesisBlockJob('indexGenesisTokenBalances', {
+						address,
+						tokenID,
+						availableBalance,
+					});
+
+					// eslint-disable-next-line no-restricted-syntax
+					for (let k = 0; k < lockedBalances.length; k++) {
+						const lockedBalance = lockedBalances[k];
+						if (!lockedChangeMap[tokenID]) lockedChangeMap[tokenID] = BigInt(0);
+						lockedChangeMap[tokenID] += BigInt(lockedBalance.amount);
+
+						// Add entry to index the genesis token locked
+						await addGenesisBlockJob('indexGenesisTokenLocked', {
+							address,
+							tokenID,
+							module: lockedBalance.module,
+							amount: lockedBalance.amount,
+						});
+					}
+
+					numLockedEntries += lockedBalances.length;
+				}
+				numBalanceEntries += userSubStoreInfos.length;
+
+				const percent =
+					totalUsers > 0 ? (((offset + BATCH_SIZE) / totalUsers) * 100).toFixed(1) : 0;
+				logger.info(
+					`Scheduled ${Math.min(
+						offset + BATCH_SIZE,
+						totalUsers,
+					)} of ${totalUsers} user item (${percent}%)`,
+				);
+
+				offset += BATCH_SIZE;
+			} catch (err) {
+				await new Promise(resolve => setTimeout(resolve, BATCH_RETRY_DELAY));
+				logger.warn(
+					`Retrying indexGenesisTokenBalances & indexGenesisTokenLocked batch starting at offset ${offset}...`,
+				);
 			}
-
-			numLockedEntries += lockedBalances.length;
 		}
 
-		for (let i = 0; i < supplySubstoreInfos.length; i++) {
-			const { tokenID, totalSupply } = supplySubstoreInfos[i];
-
-			// Add entry to index the genesis token supply
-			await addGenesisBlockJob('indexGenesisTokenSupply', {
-				tokenID,
-				totalSupply,
-			});
-		}
-
-		for (let i = 0; i < escrowSubstoreInfos.length; i++) {
-			const { escrowChainID, tokenID, amount } = escrowSubstoreInfos[i];
-
-			// Add entry to index the genesis token escrowed
-			await addGenesisBlockJob('indexGenesisTokenEscrowed', {
-				escrowChainID,
-				tokenID,
-				amount,
-			});
-		}
-
-		if (supportedSubstoreInfos.length === 1 && supportedSubstoreInfos[0].chainID === '') {
-			const { chainID, supportedTokenIDs } = supportedSubstoreInfos[0];
-
-			await addGenesisBlockJob('indexGenesisTokenSupportAllTokens', {
-				chainID,
-				supportedTokenIDs,
-			});
-		} else {
-			for (let i = 0; i < supportedSubstoreInfos.length; i++) {
-				const { chainID, supportedTokenIDs } = supportedSubstoreInfos[i];
-
-				// Add entry to index the genesis token supported
-				await addGenesisBlockJob('indexGenesisTokenSupported', {
-					chainID,
-					supportedTokenIDs,
+		for (let offset = 0; offset < totalSupplyItem; ) {
+			try {
+				const tokenSupplyModuleData = await requestConnector('getGenesisAssetByModule', {
+					module: MODULE.TOKEN,
+					subStore: MODULE_SUB_STORE.TOKEN.SUPPLY,
+					limit: BATCH_SIZE,
+					offset,
 				});
+
+				const supplySubstoreInfos = tokenSupplyModuleData[MODULE_SUB_STORE.TOKEN.SUPPLY];
+
+				for (let i = 0; i < supplySubstoreInfos.length; i++) {
+					const { tokenID, totalSupply } = supplySubstoreInfos[i];
+
+					// Add entry to index the genesis token supply
+					await addGenesisBlockJob('indexGenesisTokenSupply', {
+						tokenID,
+						totalSupply,
+					});
+				}
+
+				numSupplyEntries += supplySubstoreInfos.length;
+
+				const percent =
+					totalSupplyItem > 0 ? (((offset + BATCH_SIZE) / totalSupplyItem) * 100).toFixed(1) : 0;
+				logger.info(
+					`Scheduled ${Math.min(
+						offset + BATCH_SIZE,
+						totalSupplyItem,
+					)} of ${totalSupplyItem} supply item (${percent}%)`,
+				);
+
+				offset += BATCH_SIZE;
+			} catch (err) {
+				await new Promise(resolve => setTimeout(resolve, BATCH_RETRY_DELAY));
+				logger.warn(`Retrying indexGenesisTokenSupply batch starting at offset ${offset}...`);
+			}
+		}
+
+		for (let offset = 0; offset < totalEscrowItem; ) {
+			try {
+				const tokenEscrowedModuleData = await requestConnector('getGenesisAssetByModule', {
+					module: MODULE.TOKEN,
+					subStore: MODULE_SUB_STORE.TOKEN.ESCROW,
+					limit: BATCH_SIZE,
+					offset,
+				});
+
+				const escrowSubstoreInfos = tokenEscrowedModuleData[MODULE_SUB_STORE.TOKEN.ESCROW];
+
+				for (let i = 0; i < escrowSubstoreInfos.length; i++) {
+					const { escrowChainID, tokenID, amount } = escrowSubstoreInfos[i];
+
+					// Add entry to index the genesis token escrowed
+					await addGenesisBlockJob('indexGenesisTokenEscrowed', {
+						escrowChainID,
+						tokenID,
+						amount,
+					});
+				}
+
+				numEscrowedEntries += escrowSubstoreInfos.length;
+
+				const percent =
+					totalEscrowItem > 0 ? (((offset + BATCH_SIZE) / totalEscrowItem) * 100).toFixed(1) : 0;
+				logger.info(
+					`Scheduled ${Math.min(
+						offset + BATCH_SIZE,
+						totalEscrowItem,
+					)} of ${totalEscrowItem} escrow item (${percent}%)`,
+				);
+
+				offset += BATCH_SIZE;
+			} catch (err) {
+				await new Promise(resolve => setTimeout(resolve, BATCH_RETRY_DELAY));
+				logger.warn(`Retrying indexGenesisTokenEscrowed batch starting at offset ${offset}...`);
+			}
+		}
+
+		for (let offset = 0; offset < totalSupportedItem; ) {
+			try {
+				const tokenSupportedModuleData = await requestConnector('getGenesisAssetByModule', {
+					module: MODULE.TOKEN,
+					subStore: MODULE_SUB_STORE.TOKEN.SUPPORTED,
+					limit: BATCH_SIZE,
+					offset,
+				});
+
+				const supportedSubstoreInfos = tokenSupportedModuleData[MODULE_SUB_STORE.TOKEN.SUPPORTED];
+
+				if (supportedSubstoreInfos.length === 1 && supportedSubstoreInfos[0].chainID === '') {
+					const { chainID, supportedTokenIDs } = supportedSubstoreInfos[0];
+
+					await addGenesisBlockJob('indexGenesisTokenSupportAllTokens', {
+						chainID,
+						supportedTokenIDs,
+					});
+				} else {
+					for (let i = 0; i < supportedSubstoreInfos.length; i++) {
+						const { chainID, supportedTokenIDs } = supportedSubstoreInfos[i];
+
+						// Add entry to index the genesis token supported
+						await addGenesisBlockJob('indexGenesisTokenSupported', {
+							chainID,
+							supportedTokenIDs,
+						});
+					}
+				}
+
+				numSupportedEntries += supportedSubstoreInfos.length;
+
+				const percent =
+					totalSupportedItem > 0
+						? (((offset + BATCH_SIZE) / totalSupportedItem) * 100).toFixed(1)
+						: 0;
+				logger.info(
+					`Scheduled ${Math.min(
+						offset + BATCH_SIZE,
+						totalSupportedItem,
+					)} of ${totalSupportedItem} supported token item (${percent}%)`,
+				);
+
+				offset += BATCH_SIZE;
+			} catch (err) {
+				await new Promise(resolve => setTimeout(resolve, BATCH_RETRY_DELAY));
+				logger.warn(
+					`Retrying indexGenesisTokenSupportAllTokens & indexGenesisTokenSupported batch starting at offset ${offset}...`,
+				);
 			}
 		}
 
@@ -155,7 +249,7 @@ const indexTokenModuleAssets = async dbTrx => {
 		}
 
 		logger.info(
-			`Finished scheduling genesis account indexing jobs. Added: ${userSubStoreInfos.length} balance entries, ${numLockedEntries} locked entries, ${supplySubstoreInfos.length} supply entries, ${escrowSubstoreInfos.length} escrow entries, ${supportedSubstoreInfos.length} supported entries.`,
+			`Finished scheduling genesis account indexing jobs. Added: ${numBalanceEntries} balance entries, ${numLockedEntries} locked entries, ${numSupplyEntries} supply entries, ${numEscrowedEntries} escrow entries, ${numSupportedEntries} supported entries.`,
 		);
 	}
 
